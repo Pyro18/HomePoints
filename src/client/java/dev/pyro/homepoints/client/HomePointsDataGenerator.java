defe95d184b988a2c3ca33fad9a655e4efcf5ffc
Pyro18 <marius.noroaca@gmail.com>
2025-10-20 17:23:18 +0200

    initialize HomePoints mod with Gradle setup and essential files

---
+++ src/client/java/dev/pyro/homepoints/client/HomePointsDataGenerator.java
@@ -0,0 +1,12 @@
+package dev.pyro.homepoints.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HomePointsDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
---
+++ src/main/java/dev/pyro/homepoints/HomePoints.java
@@ -0,0 +1,10 @@
+package dev.pyro.homepoints;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HomePoints implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
